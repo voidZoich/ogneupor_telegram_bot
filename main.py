070642814bbe00e4021534a6a3dcbc1ca11c46b8
Konstantin Nikitin <nikitin.auto@gmail.com>
2021-03-04 14:59:36 +0500

    Добавил команду пингования

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
+import logging
+from config import *
+from ping import *
+
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
 
-updater = Updater(token='1592193999:AAGpywOvef3CUIatv_-Xie0zrIlmsw_ZzVY', use_context=True)
+updater = Updater(token=TOKEN, workers=4)
 dispatcher = updater.dispatcher
-import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    reply_keyboard = [['Пинг']]
+    update.message.reply_text(
+        'Здравствуйте, я бот АСУ ТП Огнеупор, введите команду',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True),
+    )
 
 
+def ping(update, context):
+    for host_name, time in pinger():
+        emoji = '❌' if time is None else '✅'
+        update.message.reply_text(
+            f"{emoji} {host_name} - {time} сек.",
+            reply_markup=ReplyKeyboardRemove()
+        )
 
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Неизвестная команда")
 
 
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+def main():
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    ping_handler = MessageHandler(Filters.regex('^(Пинг|пинг|Ping|ping)$'), ping)
+    dispatcher.add_handler(ping_handler)
+
+    unknown_handler = MessageHandler(Filters.command, unknown)
+    dispatcher.add_handler(unknown_handler)
+
+    updater.start_polling()
+    updater.idle()
 
 
-from telegram.ext import MessageHandler, Filters
-
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
-
-updater.start_polling()
+if __name__ == '__main__':
+    main()
